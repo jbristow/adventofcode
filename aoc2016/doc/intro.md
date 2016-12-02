610c71558e0bfeeb93f8895e8eab12000b6c5aef
jbristow <Jonathan_Bristow@intuit.com>
2016-12-02 14:31:54 -0800

    cleanup

--- aoc2016/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to aoc2016
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- aoc2016/src/aoc2016/day02.clj
+++ aoc2016/src/aoc2016/day02.clj
@@ -33,20 +33,23 @@
 (def left-from {3 2, 4 3, 6 5, 7 6, 8 7, 9 8, \B \A, \C \B})
 (def right-from {2 3, 3 4, 5 6, 6 7, 7 8, 8 9, \A \B, \B \C})
 
+(defn get-next-key [keymap number]
+  (if (contains? keymap number) (get keymap number) number))
+
 (defmulti diamond-move (fn [direction number] direction))
 
 (defmethod diamond-move \U [direction number]
-  (if (contains? up-from number) (get up-from number) number))
+  (get-next-key up-from number))
 (defmethod diamond-move \D [direction number]
-  (if (contains? down-from number) (get down-from number) number))
+  (get-next-key down-from number))
 (defmethod diamond-move \R [direction number]
-  (if (contains? right-from number) (get right-from number) number))
+  (get-next-key right-from number))
 (defmethod diamond-move \L [direction number]
-  (if (contains? left-from number) (get left-from number) number))
+  (get-next-key left-from number))
 
 (defn process-b [input]
   (reduce (fn [sum value] (diamond-move value sum)) 5 input))
 
 (defn answer-b [filename]
-  (clojure.string/join 
+  (clojure.string/join
    (map process-b (line-seq (BufferedReader. (FileReader. filename))))))
