dde01d4105c01ff37c7a058b29ab91ac24606c4c
jbristow <Jonathan_Bristow@intuit.com>
2016-12-05 07:54:13 -0800

    day 5 parts 1 and 2

---
+++ aoc2016/src/aoc2016/day05.clj
@@ -0,0 +1,46 @@
+(ns aoc2016.day05
+  (:import (java.security MessageDigest)))
+
+(defn md5 [s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        size (* 2 (.getDigestLength algorithm))
+        raw (.digest algorithm (.getBytes s))
+        sig (.toString (BigInteger. 1 raw) 16)
+        padding (apply str (repeat (- size (count sig)) "0"))]
+    (str padding sig)))
+
+(defn iterations [initial]
+  (map #(md5 (str initial %)) (range)))
+
+(defn valid-hashes [input] (filter #(clojure.string/starts-with? % "00000")
+                                   (iterations input)))
+
+(defn answer [input]
+  (apply str
+         (take 8
+               (map #(nth % 5)
+                    (valid-hashes input)))))
+
+(defn answer-b [input]
+  (let [hashes (valid-hashes input)]
+    (apply str
+           (map second
+                (sort-by first
+                         (into (list)
+                               (loop [h (first hashes)
+                                      r (rest hashes)
+                                      seen {}]
+                                 (let [i (Integer/valueOf (str (nth h 5)) 16)
+                                       v (nth h 6)]
+                                   (println h i v (contains? seen i)
+                                            (<= 8 i))
+                                   (cond
+                                     (= 8 (count seen))
+                                     seen
+
+                                     (or (contains? seen i)
+                                         (<= 8 i))
+                                     (recur (first r) (rest r) seen)
+
+                                     :else
+                                     (recur (first r) (rest r) (merge seen {i v})))))))))))
