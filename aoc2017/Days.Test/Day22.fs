40a508e6b8e2dcdf40a975cc5f17ea82b6af0856
Jon Bristow <jon.bristow@teradata.com>
2017-12-21 23:43:08 -0800

    2017 day 22

---
+++ aoc2017/Days.Test/Day22.fs
@@ -0,0 +1,50 @@
+module Tests.Day22
+
+open System
+open NUnit.Framework
+open Swensen.Unquote
+open Day22
+
+[<Test>]
+let testSimpleParse() =
+  Part1.parseInput [|"...";"...";"..."|] =! Map.ofList  [((-1, -1), false); ((-1, 0), false); ((-1, 1), false); ((0, -1), false); ((0, 0), false); ((0, 1), false); ((1, -1), false); ((1, 0), false); ((1, 1), false)]
+
+[<Test>]
+let testSamplePart1_70() =
+  let c = Part1.simulate 70 [| "..#"; "#.."; "..."; |]
+  printfn "%A" c
+  c.Infected =! 41
+
+[<Test>]
+let testSamplePart1_10k() =
+  let c = Part1.simulate 10000 [| "..#"; "#.."; "..."; |]
+  printfn "%A" c
+  c.Infected =! 5587
+
+[<Test>]
+let testAnswerPart1() =
+  let data : string array = System.IO.File.ReadAllLines("day22.txt")
+
+  let c = data |> Part1.simulate 10000
+  printfn "%A" c
+  c.Infected =! 5462
+
+[<Test>]
+let testSamplePart2_100() =
+  let c = Part2.simulate 100 [| "..#"; "#.."; "..."; |]
+  printfn "%A" c
+  c.Infected =! 26
+
+[<Test>]
+let testSamplePart2_10m() =
+  let c = Part2.simulate 10000000 [| "..#"; "#.."; "..."; |]
+  printfn "%A" c
+  c.Infected =! 2511944
+
+[<Test>]
+let testAnswerPart2() =
+  let data : string array = System.IO.File.ReadAllLines("day22.txt")
+
+  let c = data |> Part2.simulate 10000000
+  printfn "%A" c
+  c.Infected =! 5587
---
+++ aoc2017/Days/Day22.fs
@@ -0,0 +1,189 @@
+module Day22
+
+type Direction =
+    | North
+    | South
+    | East
+    | West
+
+type State =
+    | Clean
+    | Weakened
+    | Infected
+    | Flagged
+
+module State =
+    let update =
+        function
+        | Some Clean | None -> Weakened
+        | Some Weakened -> Infected
+        | Some Infected -> Flagged
+        | Some Flagged -> Clean
+
+    let toChar =
+        function
+        | Some Clean | None -> '.'
+        | Some Weakened -> 'W'
+        | Some Infected -> '#'
+        | Some Flagged -> 'F'
+
+type Coordinate = int * int
+
+type Carrier =
+    { Direction : Direction
+      Position : int * int
+      Infected : int }
+
+module Direction =
+    let delta =
+        function
+        | North -> (0, -1)
+        | South -> (0, 1)
+        | East -> (1, 0)
+        | West -> (-1, 0)
+
+    let turnRight =
+        function
+        | North -> East
+        | East -> South
+        | South -> West
+        | West -> North
+
+    let turnLeft =
+        function
+        | North -> West
+        | East -> North
+        | South -> East
+        | West -> South
+
+    let reverse =
+        function
+        | North -> South
+        | South -> North
+        | East -> West
+        | West -> East
+
+module Coordinate =
+    let add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+module Carrier =
+    let init : Carrier =
+        { Direction = North
+          Position = 0, 0
+          Infected = 0 }
+
+    let updateInfected doInfect c : Carrier =
+        if doInfect then { c with Infected = c.Infected + 1 }
+        else c
+
+    let updateDirection direction c : Carrier = { c with Direction = direction }
+    let moveForward c : Carrier =
+        { c with Position =
+                     Coordinate.add c.Position (c.Direction |> Direction.delta) }
+
+module Part1 =
+    let burst grid carrier =
+        let { Direction = d; Position = (x, y) } = carrier
+
+        let newDirection, willInfect =
+            match grid |> Map.tryFind (x, y) with
+            | Some true -> d |> Direction.turnRight, false
+            | Some false | None -> d |> Direction.turnLeft, true
+
+        let nextGrid = grid |> Map.add (x, y) willInfect
+
+        let nextCarrier =
+            carrier
+            |> Carrier.updateInfected willInfect
+            |> Carrier.updateDirection newDirection
+            |> Carrier.moveForward
+        nextGrid, nextCarrier
+
+    let isInfected c = c = '#'
+
+    let parseInput (data : string array) =
+        let size = data |> Array.length
+        data
+        |> Array.mapi
+               (fun y row ->
+               row
+               |> Seq.mapi
+                      (fun x node ->
+                      ((x - size / 2, y - size / 2), isInfected node)))
+        |> Seq.concat
+        |> Map.ofSeq
+
+    let simulate maxn data =
+        let grid = data |> parseInput
+        let carrier = Carrier.init
+
+        let rec runner (g : Map<Coordinate, bool>) (c : Carrier) (n : int) =
+            match n with
+            | x when x < maxn ->
+                let nextG, nextC = burst g c
+                runner nextG nextC (n + 1)
+            | _ -> (g, c)
+
+        let _, finalC = runner grid carrier 0
+        finalC
+
+module Part2 =
+    let burst grid carrier =
+        let { Direction = d; Position = (x, y); Infected = inf } = carrier
+        let nodeState = grid |> Map.tryFind (x, y)
+
+        let newDirection =
+            match nodeState with
+            | Some Clean | None -> d |> Direction.turnLeft
+            | Some Weakened -> d
+            | Some Infected -> d |> Direction.turnRight
+            | Some Flagged -> d |> Direction.reverse
+
+        let nextNodeState = (nodeState |> State.update)
+        let nextGrid = grid |> Map.add (x, y) nextNodeState
+
+        let addInfected =
+            if (nextNodeState = Infected) then inf + 1
+            else inf
+
+        let nextCarrier =
+            { carrier with Infected = addInfected
+                           Direction = newDirection
+                           Position =
+                               (x, y)
+                               |> Coordinate.add
+                                      (newDirection |> Direction.delta) }
+
+        nextGrid, nextCarrier
+
+    let isInfected c =
+        if c = '#' then Some(Infected)
+        else None
+
+    let parseInput (data : string array) =
+        let size = data |> Array.length
+        data
+        |> Array.mapi (fun y row ->
+               row
+               |> Seq.mapi (fun x node -> ((x, y), isInfected node))
+               |> Seq.filter (fun z -> z
+                                       |> snd
+                                       <> None)
+               |> Seq.map (fun (a, b) -> a, Option.get b))
+        |> Seq.concat
+        |> Map.ofSeq
+
+    let simulate maxn data =
+        let grid = data |> parseInput
+        let size = data |> Array.length
+        let carrier = { Carrier.init with Position = (size / 2, size / 2) }
+
+        let rec runner (g : Map<Coordinate, State>) (c : Carrier) (n : int) =
+            match n with
+            | x when x < maxn ->
+                let nextG, nextC = burst g c
+                runner nextG nextC (n + 1)
+            | _ -> (g, c)
+
+        let _, finalC = runner grid carrier 0
+        finalC
