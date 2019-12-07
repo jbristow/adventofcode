223523cabebb9ee3910f339973a9dc8bb7179f32
Jon Bristow <jonthegm@yahoo.com>
2019-12-06 20:50:14 -0800

    Day 06

--- aoc2019/kotlin/src/main/kotlin/Day05.kt
+++ aoc2019/kotlin/src/main/kotlin/Day05.kt
@@ -1,3 +1,177 @@
+import arrow.core.Either
+import arrow.core.Option
+import arrow.core.flatMap
+import arrow.core.getOrElse
+import arrow.core.left
+import arrow.core.right
+import arrow.core.some
+import java.nio.file.Files
+import java.nio.file.Paths
+
+sealed class Mode {
+    object Immediate : Mode()
+    object Position : Mode()
+}
+
+sealed class Instruction {
+    abstract val opcodeFormat: String
+    abstract fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int>
+
+    open fun findInputs(code: Array<Int>, pointer: Int) =
+        code.drop(pointer + 1)
+            .take(3)
+            .zip(opcodeFormat.format(code[pointer] / 100).map {
+                when (it) {
+                    '0' -> Mode.Position
+                    '1' -> Mode.Immediate
+                    else -> throw Error("Bad mode $it")
+                }
+            }.reversed())
+            .map { (it, mode) ->
+                when (mode) {
+                    Mode.Position -> code[it]
+                    Mode.Immediate -> it
+                }
+            }
+
+    sealed class ThreeParameterInstruction : Instruction() {
+        override val opcodeFormat = "1%02d"
+
+        class Add : ThreeParameterInstruction() {
+            override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> {
+                code[params[2]] = params[0] + params[1]
+                return (pointer + 4).right()
+            }
+        }
+
+        class Multiply : ThreeParameterInstruction() {
+            override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> {
+                code[params[2]] = params[0] * params[1]
+                return (pointer + 4).right()
+            }
+        }
+
+        class LessThan : ThreeParameterInstruction() {
+            override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> {
+                code[params[2]] = when {
+                    params[0] < params[1] -> 1
+                    else -> 0
+                }
+                return (pointer + 4).right()
+            }
+        }
+
+        class Equal : ThreeParameterInstruction() {
+            override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> {
+                code[params[2]] = when {
+                    params[0] == params[1] -> 1
+                    else -> 0
+                }
+                return (pointer + 4).right()
+            }
+        }
+    }
+
+    sealed class TwoParameterInstruction : Instruction() {
+        override val opcodeFormat = "%02d"
+
+        class JumpIfTrue : TwoParameterInstruction() {
+            override fun execute(pointer: Int, code: Array<Int>, params: List<Int>) =
+                when (params[0]) {
+                    0 -> pointer + 3
+                    else -> params[1]
+                }.right()
+        }
+
+        class JumpIfFalse : TwoParameterInstruction() {
+            override fun execute(pointer: Int, code: Array<Int>, params: List<Int>) =
+                when (params[0]) {
+                    0 -> params[1]
+                    else -> pointer + 3
+                }.right()
+        }
+    }
+
+    class SetFromInput(private val input: Int) : Instruction() {
+        override val opcodeFormat = "1"
+        override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> {
+            code[params[0]] = input
+            return (pointer + 2).right()
+        }
+    }
+
+    class Output : Instruction() {
+        override val opcodeFormat = "0"
+        override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> {
+            println("output: ${params[0]}")
+            return (pointer + 2).right()
+        }
+    }
+
+    object End : Instruction() {
+        override val opcodeFormat: String
+            get() = throw Error("No opcode format for End instructions.")
+
+        override fun findInputs(code: Array<Int>, pointer: Int) = emptyList<Int>()
+        override fun execute(pointer: Int, code: Array<Int>, params: List<Int>): Either<Option<String>, Int> =
+            Option.empty<String>().left()
+    }
+}
+
 object Day05 {
 
+    private fun parseInstruction(instruction: String, input: Int) =
+        when (instruction.takeLast(2).toInt()) {
+            1 -> Instruction.ThreeParameterInstruction.Add().right()
+            2 -> Instruction.ThreeParameterInstruction.Multiply().right()
+            3 -> Instruction.SetFromInput(input).right()
+            4 -> Instruction.Output().right()
+            5 -> Instruction.TwoParameterInstruction.JumpIfTrue().right()
+            6 -> Instruction.TwoParameterInstruction.JumpIfFalse().right()
+            7 -> Instruction.ThreeParameterInstruction.LessThan().right()
+            8 -> Instruction.ThreeParameterInstruction.Equal().right()
+            99 -> Instruction.End.right()
+            else -> "Problem parsing instruction $instruction".some().left()
+        }
+
+    private fun handleCodePoint(pointer: Int, input: Int, code: Array<Int>) =
+        parseInstruction(code[pointer].toString(), input).flatMap { instr ->
+            instr.execute(pointer, code, instr.findInputs(code, pointer))
+        }
+
+    tailrec fun step(
+        code: Array<Int>,
+        input: Int,
+        instructionPointer: Either<Option<String>, Int> = Either.right(0)
+    ): String = when (instructionPointer) {
+        is Either.Left<Option<String>> -> instructionPointer.a.getOrElse { "Program terminated successfully." }
+        is Either.Right<Int> -> {
+            val nextInstruction = handleCodePoint(instructionPointer.b, input, code)
+            step(code, input, nextInstruction)
+        }
+    }
+
+    const val FILENAME = "src/main/resources/day05.txt"
+}
+
+fun main() {
+    val problemInput = Files.readAllLines(Paths.get(Day05.FILENAME))
+        .first()
+        .split(",")
+        .map { it.toInt() }
+
+    // Part 01
+    println("Part 01")
+    Day05.step(code = problemInput.toTypedArray(), input = 1)
+
+    // Part 02
+    println("\nPart 02")
+    Day05.step(code = problemInput.toTypedArray(), input = 5)
+
+    println("\nDay 02")
+    val day02Code = Files.readAllLines(Paths.get("src/main/resources/day02.txt")).first()
+        .split(",")
+        .map { it.toInt() }.toTypedArray()
+    Day05.step(code = day02Code, input = 5)
+    println(day02Code.take(10))
 }
---
+++ aoc2019/kotlin/src/main/kotlin/Day06.kt
@@ -0,0 +1,77 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+object Day06 {
+
+    private const val FILENAME = "src/main/resources/day06.txt"
+
+    private fun parseOrbit(s: String): Pair<String, String> {
+        return """(.*)\)(.*)""".toRegex().matchEntire(s)?.let {
+            it.groupValues[1] to it.groupValues[2]
+        } ?: throw Error("Bad orbit: $s")
+    }
+
+
+    private fun List<String>.countPaths() =
+        map(::parseOrbit).fold(mapOf<String, Set<String>>()) { m, (a, b) ->
+            val nset = (m[a] ?: emptySet()) + b
+            m + (a to nset)
+        }.let { orbits ->
+            orbits.countChildren(orbits["COM"].orEmpty().map { it to 1 }, 0)
+        }
+
+    private tailrec fun Map<String, Set<String>>.countChildren(
+        paths: List<Pair<String, Int>>,
+        size: Int
+    ): Int {
+
+        val nextPaths = paths.flatMap {
+            this[it.first].orEmpty().map { c -> c to it.second + 1 }
+        }
+        return when {
+            nextPaths.isEmpty() -> size + paths.sumBy(Pair<String, Int>::second)
+            else -> countChildren(nextPaths, size + paths.sumBy(Pair<String, Int>::second))
+        }
+
+    }
+
+    private fun List<String>.shortestDistanceToSanta(): Int {
+        val paths = map(::parseOrbit).fold(mutableMapOf<String, Set<String>>()) { m, (a, b) ->
+            m[a] = (m[a] ?: emptySet()) + b
+            m
+        }
+
+        val youParents = paths.filterValues { "YOU" in it }.keys.map { it to 0 }
+
+        return paths.findDistanceToSan(queue = LinkedList(youParents), seen = setOf("YOU"))
+
+    }
+
+    private tailrec fun MutableMap<String, Set<String>>.findDistanceToSan(
+        queue: Deque<Pair<String, Int>>,
+        seen: Set<String>
+    ): Int {
+        val (node, dist) = queue.pop()
+        return when (node) {
+            "SAN" -> dist - 1
+            in seen -> findDistanceToSan(queue, seen)
+            else -> {
+                queue.addAll(filterValues { node in it }.keys.map { it to (dist + 1) })
+                queue.addAll(filterKeys { node == it }.flatMap { (_, v) -> v.map { it to (dist + 1) } })
+                findDistanceToSan(LinkedList(queue.filterNot { (n, _) -> n in seen }), seen + node)
+            }
+        }
+    }
+
+    fun part1() = Files.readAllLines(Paths.get(FILENAME)).countPaths()
+
+    fun part2(): Int = Files.readAllLines(Paths.get(FILENAME)).shortestDistanceToSanta()
+
+}
+
+
+fun main() {
+    println(Day06.part1())
+    println(Day06.part2())
+}
