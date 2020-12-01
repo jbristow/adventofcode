e71e69807a2319f47fe2d1a345bbb934286b5d50
Jon Bristow <jonthegm@yahoo.com>
2020-11-30 20:21:54 -0800

    initial 2020 setup commit

---
+++ aoc2020/kotlin/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+    kotlin("kapt") version "1.4.20"
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val arrowVersion = "0.11.0"
+val jupiterVersion = "5.7.0"
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+    implementation("io.arrow-kt:arrow-optics:$arrowVersion")
+    kapt("io.arrow-kt:arrow-meta:$arrowVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    // Use the Kotlin test library
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
+
+    // Use the Kotlin JUnit integration
+    testImplementation(kotlin("test-junit"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$jupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
+    testCompile("org.assertj:assertj-core:3.18.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ aoc2020/kotlin/src/main/kotlin/Day01.kt
@@ -0,0 +1,36 @@
+import Day01.part1
+import Day01.part2
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.math.max
+
+object Day01 {
+    private const val FILENAME = "src/main/resources/day01.txt"
+
+    fun fuelNeeded(mass: Int) = max((mass / 3) - 2, 0)
+
+    tailrec fun totalFuelNeeded(mass: Int, fuel: Int? = null, totalFuel: Int = 0): Int {
+
+        val nextFuel = when (fuel) {
+            null -> fuelNeeded(mass)
+            else -> fuelNeeded(fuel)
+        }
+        return when (nextFuel) {
+            0 -> totalFuel
+            else -> totalFuelNeeded(mass, nextFuel, totalFuel + nextFuel)
+        }
+    }
+
+    fun part1() = Files.readAllLines(Paths.get(FILENAME))
+        .sumBy { fuelNeeded(it.toInt()) }
+        .toString()
+
+    fun part2() = Files.readAllLines(Paths.get(FILENAME))
+        .sumBy { totalFuelNeeded(it.toInt()) }
+        .toString()
+}
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
