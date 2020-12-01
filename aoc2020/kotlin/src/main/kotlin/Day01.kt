5410e8bf046c026f37b5e90f5a3af818d0a3511f
Jon Bristow <jon.bristow@defensestorm.com>
2020-11-30 21:22:40 -0800

    2020 day01

--- aoc2020/kotlin/src/main/kotlin/Day01.kt
+++ aoc2020/kotlin/src/main/kotlin/Day01.kt
@@ -2,35 +2,42 @@ import Day01.part1
 import Day01.part2
 import java.nio.file.Files
 import java.nio.file.Paths
-import kotlin.math.max
 
 object Day01 {
     private const val FILENAME = "src/main/resources/day01.txt"
 
-    fun fuelNeeded(mass: Int) = max((mass / 3) - 2, 0)
+    private fun find2020(lines: List<String>) =
+            (lines.indices).asSequence().mapNotNull { i ->
+                val b = (i until lines.size).find { j ->
+                    lines[j].toInt() + lines[i].toInt() == 2020
+                }
+                when (b) {
+                    null -> null
+                    else -> lines[b].toInt().times(lines[i].toInt())
+                }
+            }.first()
 
-    tailrec fun totalFuelNeeded(mass: Int, fuel: Int? = null, totalFuel: Int = 0): Int {
+    private fun find2020From3(lines: List<String>): Int? {
+        val x = (lines.indices).asSequence().flatMap { i ->
+            (i until lines.size).asSequence().flatMap { j ->
+                (j until lines.size).asSequence().map { k ->
+                    listOf(i, j, k)
+                }
+            }
+        }.filter { ls ->
+            ls.map { lines[it].toInt() }.sum() == 2020
+        }.first()
 
-        val nextFuel = when (fuel) {
-            null -> fuelNeeded(mass)
-            else -> fuelNeeded(fuel)
-        }
-        return when (nextFuel) {
-            0 -> totalFuel
-            else -> totalFuelNeeded(mass, nextFuel, totalFuel + nextFuel)
-        }
+        return lines[x[0]].toInt() * lines[x[1]].toInt() * lines[x[2]].toInt()
     }
 
-    fun part1() = Files.readAllLines(Paths.get(FILENAME))
-        .sumBy { fuelNeeded(it.toInt()) }
-        .toString()
+    fun part1(): Int? = find2020(Files.readAllLines(Paths.get(FILENAME)))
 
-    fun part2() = Files.readAllLines(Paths.get(FILENAME))
-        .sumBy { totalFuelNeeded(it.toInt()) }
-        .toString()
+    fun part2(): Int? = find2020From3(Files.readAllLines(Paths.get(FILENAME)))
 }
 
 fun main() {
     println("Part 1: ${part1()}")
     println("Part 2: ${part2()}")
 }
+
