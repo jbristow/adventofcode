f6854745f1bc54a96854bd3f01084cd44cadc9f0
Jon Bristow <jon.bristow@defensestorm.com>
2020-12-01 21:39:39 -0800

    2020 day02

--- aoc2020/kotlin/src/main/kotlin/Day01.kt
+++ aoc2020/kotlin/src/main/kotlin/Day01.kt
@@ -6,34 +6,41 @@ import java.nio.file.Paths
 object Day01 {
     private const val FILENAME = "src/main/resources/day01.txt"
 
-    private fun find2020(lines: List<String>) =
-            (lines.indices).asSequence().mapNotNull { i ->
-                val b = (i until lines.size).find { j ->
-                    lines[j].toInt() + lines[i].toInt() == 2020
-                }
-                when (b) {
-                    null -> null
-                    else -> lines[b].toInt().times(lines[i].toInt())
-                }
-            }.first()
+    private fun find2020(lines: List<Int>): Int {
+        val sorted = lines.sorted()
 
-    private fun find2020From3(lines: List<String>): Int? {
-        val x = (lines.indices).asSequence().flatMap { i ->
+        tailrec fun recurse(minVal: Int, remaining: List<Int>): Int {
+            val answer = minVal + remaining.last()
+            return when {
+                answer > 2020 -> recurse(minVal, remaining.dropLast(1))
+                answer == 2020 -> minVal * remaining.last()
+                else -> recurse(remaining.first(), remaining.drop(1))
+            }
+        }
+
+        return recurse(sorted.first(), sorted.drop(1))
+    }
+
+
+    private fun find2020From3(lines: List<Int>): Int {
+        val tuple = (lines.indices).asSequence().flatMap { i ->
             (i until lines.size).asSequence().flatMap { j ->
                 (j until lines.size).asSequence().map { k ->
                     listOf(i, j, k)
+                }.filter { ls ->
+                    ls.map { lines[it] }.sum() == 2020
                 }
             }
-        }.filter { ls ->
-            ls.map { lines[it].toInt() }.sum() == 2020
         }.first()
 
-        return lines[x[0]].toInt() * lines[x[1]].toInt() * lines[x[2]].toInt()
+        val (x, y, z) = tuple
+
+        return lines[x] * lines[y] * lines[z]
     }
 
-    fun part1(): Int? = find2020(Files.readAllLines(Paths.get(FILENAME)))
+    fun part1() = find2020(Files.readAllLines(Paths.get(FILENAME)).map(String::toInt))
 
-    fun part2(): Int? = find2020From3(Files.readAllLines(Paths.get(FILENAME)))
+    fun part2() = find2020From3(Files.readAllLines(Paths.get(FILENAME)).map(String::toInt))
 }
 
 fun main() {
---
+++ aoc2020/kotlin/src/main/kotlin/Day02.kt
@@ -0,0 +1,36 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private const val FILENAME = "src/main/resources/day02.txt"
+
+object Day02 {
+
+    fun validateLinePart1(input: String): Boolean {
+        val groups = """(\d+)-(\d+) (.): (.*)""".toRegex().matchEntire(input)!!.groupValues
+        val minCount = groups[1].toInt()
+        val maxCount = groups[2].toInt()
+        val matchChar = groups[3].first()
+        val password = groups[4]
+
+        return password.count(matchChar::equals) in minCount..maxCount
+
+    }
+
+    fun validateLinePart2(input: String): Boolean {
+        val groups = """(\d+)-(\d+) (.): (.*)""".toRegex().matchEntire(input)!!.groupValues
+        val first = groups[1].toInt() - 1
+        val last = groups[2].toInt() - 1
+        val matchChar = groups[3].first()
+        val password = groups[4]
+
+        return (password[first] == matchChar && password[last] != matchChar)
+                || (password[first] != matchChar && password[last] == matchChar)
+
+    }
+}
+
+fun main() {
+    val lines = Files.readAllLines(Paths.get(FILENAME))
+    println("Part1: ${lines.count(Day02::validateLinePart1)}")
+    println("Part2: ${lines.count(Day02::validateLinePart2)}")
+}
