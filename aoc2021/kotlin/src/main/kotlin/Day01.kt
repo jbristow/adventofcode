bf4b03104db82b9c6542e5d4aa212c48e1c16916
Jon Bristow <jon.bristow@defensestorm.com>
2021-11-30 21:30:43 -0800

    2021 day 01

--- aoc2021/kotlin/src/main/kotlin/Day01.kt
+++ aoc2021/kotlin/src/main/kotlin/Day01.kt
@@ -1,20 +1,18 @@
 import util.AdventOfCode
 
 object Day01 : AdventOfCode() {
-    fun part1(input: List<Int>): Int {
-        return -1
-    }
 
-    fun part2(input: List<Int>): Int {
-        return -1
-    }
+    fun part1(input: List<Int>) =
+        input.windowed(2, 1).count { (a, b) -> a < b }
+
+    fun part2(input: List<Int>) =
+        part1(input.windowed(3, 1).map(List<Int>::sum))
 
     @JvmStatic
     fun main(args: Array<String>) {
         println("Day 1")
-        println(inputFileString)
-        println("\tPart 1: ${timed {part1(inputFileInts)}}")
-        println("\tPart 2: ${timed {part2(inputFileInts)}}")
+        println("\tPart 1: ${part1(inputFileInts)}")
+        println("\tPart 2: ${part2(inputFileInts)}")
     }
 }
 
--- aoc2021/kotlin/src/test/kotlin/Day01Test.kt
+++ aoc2021/kotlin/src/test/kotlin/Day01Test.kt
@@ -1,18 +1,30 @@
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 
 internal class Day01Test {
 
     @Nested
     inner class Examples {
+        private val sample = """199
+                               |200
+                               |208
+                               |210
+                               |200
+                               |207
+                               |240
+                               |269
+                               |260
+                               |263""".trimMargin().lines().map(String::toInt)
+
         @Test
         fun part1() {
+            assertThat(Day01.part1(sample)).isEqualTo(7)
         }
 
         @Test
         fun part2() {
+            assertThat(Day01.part2(sample)).isEqualTo(5)
         }
     }
 
@@ -21,10 +33,12 @@ internal class Day01Test {
 
         @Test
         fun part1() {
+            assertThat(Day01.part1(Day01.inputFileInts)).isEqualTo(1564)
         }
 
         @Test
         fun part2() {
+            assertThat(Day01.part2(Day01.inputFileInts)).isEqualTo(1611)
         }
 
     }
