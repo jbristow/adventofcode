3317a7e4cab7ad7fba75de8791f3e07521d7f578
Jon Bristow <jon.bristow@defensestorm.com>
2022-12-02 21:30:36 -0800

    aoc2022 - Day 3 - kotlin

---
+++ aoc2022/kotlin/src/main/kotlin/Day03.kt
@@ -0,0 +1,24 @@
+import util.AdventOfCode
+
+object Day03 : AdventOfCode() {
+    fun Char.priority() =
+        this.code - if (this.isLowerCase()) 96 else 38
+
+    fun part1(input: Sequence<String>) =
+        input.flatMap {
+            it.substring(0, it.length / 2).toSet().intersect(it.substring(it.length / 2).toSet())
+        }.sumOf { it.priority() }
+
+    fun part2(input: Sequence<String>) =
+        input.map { it.toSet() }
+            .chunked(3)
+            .flatMap { group -> group.reduce { acc, curr -> acc.intersect(curr) } }
+            .sumOf { it.priority() }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Day 3")
+        println("\tPart 1: ${part1(inputFileLineSequence)}")
+        println("\tPart 2: ${part2(inputFileLineSequence)}")
+    }
+}
