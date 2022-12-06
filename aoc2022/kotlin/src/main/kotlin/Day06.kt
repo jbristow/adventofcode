89e41026689ecc1eff56d6aa2b7ba53f06cd33e5
Jon Bristow <jon.bristow@defensestorm.com>
2022-12-05 21:25:29 -0800

    aoc2022 - day06 - kotlin

---
+++ aoc2022/kotlin/src/main/kotlin/Day06.kt
@@ -0,0 +1,25 @@
+import util.AdventOfCode
+
+object Day06 : AdventOfCode() {
+
+    private fun <T> List<T>.isUnique() = this.toSet().size == this.size
+
+    private fun <T> Sequence<T>.findFirstUniqueOfSize(size: Int) =
+        this.windowed(size)
+            .withIndex()
+            .find { (_, line) -> line.isUnique() }
+            ?.index
+            ?.plus(size)
+
+    fun part1(input: String) = input.asSequence().findFirstUniqueOfSize(4)
+
+    fun part2(input: String) = input.asSequence().findFirstUniqueOfSize(14)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Day 5")
+        println("\tPart 1: ${part1(inputFileString)}")
+        println("\tPart 2: ${part2(inputFileString)}")
+    }
+
+}
