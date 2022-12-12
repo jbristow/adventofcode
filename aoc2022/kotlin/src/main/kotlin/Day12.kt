e8cf585c7a29fb9c0513ae3b0faf5713be36b269
Jon Bristow <jon.bristow@defensestorm.com>
2022-12-12 09:41:09 -0800

    finally porting pathfinding to utils

--- aoc2022/kotlin/src/main/kotlin/Day12.kt
+++ aoc2022/kotlin/src/main/kotlin/Day12.kt
@@ -1,5 +1,7 @@
+import util.AStarSearch
 import util.AdventOfCode
 import util.Point2d
+import util.Point2d.Companion.manhattanDistance
 
 object Day12 : AdventOfCode() {
 
@@ -11,7 +13,7 @@ object Day12 : AdventOfCode() {
         }.toMap()
         val startLoc = heightCharMap.filterValues { it == 'S' }.keys.first()
         val endLoc = heightCharMap.filterValues { it == 'E' }.keys.first()
-        val heightMap = heightCharMap.mapValues { (k, v) ->
+        val heightMap = heightCharMap.mapValues { (_, v) ->
             when (v) {
                 'S' -> 0
                 'E' -> 'z'.code - 'a'.code
@@ -21,90 +23,33 @@ object Day12 : AdventOfCode() {
         return Topomap(heightMap, startLoc, endLoc)
     }
 
-    fun <P> djikstra(
-        start: P,
-        isEnd: (P?) -> Boolean,
-        q: Set<P>,
-        neighborFn: (P) -> List<P>
-    ): Pair<Int?, List<P>> {
-        tailrec fun djikstraPrime(
-            q: Set<P>,
-            dist: Map<P, Int>,
-            prev: Map<P, P> = emptyMap()
-        ): Pair<Int?, List<P>> {
-            tailrec fun reconstructPath(
-                end: P?,
-                output: List<P>
-            ): List<P> = when (end) {
-                null -> output + start
-                else -> reconstructPath(prev[end], output + end)
-            }
-
-            val u = q.filter { it in dist }.minByM { dist[it] }
-
-            return when (u) {
-                null -> null to emptyList()
-                else -> {
-                    if (isEnd(u)) {
-                        return dist[u] to reconstructPath(u, emptyList())
-                    }
-                    val updates =
-                        neighborFn(u).filter { v ->
-                            u in q && (dist[v] == null || dist[v]!! > ((dist[u] ?: 0) + 1))
-                        }
-                    djikstraPrime(
-                        q - u,
-                        dist + updates.map { it to (dist[u] ?: 0) + 1 },
-                        prev + updates.map { it to u }
-                    )
-                }
-            }
-        }
-        return djikstraPrime(q, mapOf(start to 0), emptyMap())
-    }
-
-    /**
-     * Returns the first element yielding the smallest value of the given function or `null` if there are no elements.
-     *
-     */
-    inline fun <T, R : Comparable<R>> Iterable<T>.minByM(selector: (T) -> R?): T? {
-        val iterator = iterator()
-        if (!iterator.hasNext()) return null
-        var minElem = iterator.next()
-        if (!iterator.hasNext()) return minElem
-        var minValue = selector(minElem)
-        do {
-            val e = iterator.next()
-            val v = selector(e)
-            if (minValue != null && v != null && minValue > v) {
-                minElem = e
-                minValue = v
-            }
-        } while (iterator.hasNext())
-        return minElem
-    }
-
     private fun part1(input: List<String>): Int {
         val (heightMap, startLoc, endLoc) = parseInput(input)
-        val output = djikstra(startLoc, { it == endLoc }, heightMap.keys) {
+        val output = AStarSearch.orthogonalShortestPath(start = startLoc, end = endLoc, spaces = heightMap.keys) {
+            val currHeight = heightMap[it]!!
             it.orthoNeighbors.filter { neighbor ->
-                neighbor in heightMap && (heightMap[neighbor]!! <= (heightMap[it]!! + 1))
-            }
+                neighbor in heightMap &&
+                        heightMap[neighbor]!! <= (currHeight + 1)
+            }.toSet()
         }
-        return output.first!!
+        return output.size
     }
 
     private fun part2(input: List<String>): Int {
         val (heightMap, _, endLoc) = parseInput(input)
-
         val startingCandidates = heightMap.filterValues { it == 0 }.keys
-
-        val output = djikstra(endLoc, { it in startingCandidates }, heightMap.keys) {
-            it.orthoNeighbors.filter { neighbor ->
-                neighbor in heightMap && (heightMap[neighbor]!! >= (heightMap[it]!! - 1))
+        val output = AStarSearch.shortestPath(
+            start = endLoc,
+            spaces = heightMap.keys,
+            isGoal = { it in startingCandidates },
+            heuristicCostEstimate = { -it.manhattanDistance(endLoc) },
+            neighbors = {
+                it.orthoNeighbors.filter { neighbor ->
+                    neighbor in heightMap && (heightMap[neighbor]!! >= (heightMap[it]!! - 1))
+                }.toSet()
             }
-        }
-        return output.first!!
+        )
+        return output.size
     }
 
     @JvmStatic
