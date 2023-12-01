ce5f66e25b27374fdbeff1660bb72de0c40b3e9c
Jon Bristow <jon.bristow@defensestorm.com>
2023-11-30 21:41:30 -0800

    2023 day 01

--- aoc2023/kotlin/src/main/kotlin/Day01.kt
+++ aoc2023/kotlin/src/main/kotlin/Day01.kt
@@ -1,15 +1,41 @@
 import util.AdventOfCode
 
 object Day01 : AdventOfCode() {
+    private fun part1(input: List<String>) =
+        input.map { it.filter(Char::isDigit) }
+            .sumOf { "${it.first()}${it.last()}".toInt() }
 
-    fun part1(input: String) = null
+    private val numberRegex = """\d|(one|two|three|four|five|six|seven|eight|nine)""".toRegex()
 
-    fun part2(input: String) = null
+    private fun part2(input: List<String>) =
+        input.map { word ->
+            word.indices.mapNotNull { numberRegex.find(word, startIndex = it)?.value }
+        }.sumOf {
+            "${it.first().asNumber()}${it.last().asNumber()}".toInt()
+        }
 
     @JvmStatic
     fun main(args: Array<String>) {
         println("Day 1")
-        println("\tPart 1: ${part1(inputFileString)}")
-        println("\tPart 2: ${part2(inputFileString)}")
+        println("\tPart 1: ${part1(inputFileLines)}")
+        println("\tPart 2: ${part2(inputFileLines)}")
+    }
+
+    private fun String.asNumber(): Int {
+        return when (this) {
+            "one" -> 1
+            "two" -> 2
+            "three" -> 3
+            "four" -> 4
+            "five" -> 5
+            "six" -> 6
+            "seven" -> 7
+            "eight" -> 8
+            "nine" -> 9
+            else -> this.toInt()
+        }
     }
 }
+
+
+
