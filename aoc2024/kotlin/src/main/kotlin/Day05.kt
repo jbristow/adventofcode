9d1e4bf36c02bcf75d6fec6f5227154a99fed877
Jon Bristow <jonthegm@yahoo.com>
2024-12-04 22:47:52 -0800

    2024 day 05 part 2

--- aoc2024/kotlin/src/main/kotlin/Day05.kt
+++ aoc2024/kotlin/src/main/kotlin/Day05.kt
@@ -1,85 +1,66 @@
 import util.*
 
 object Day05 : AdventOfCode() {
+    private fun parse(input: String): Pair<Map<Int, List<Int>>, List<List<Int>>> {
+        val split = input.split("\n\n")
+        val before =
+            split[0]
+                .lines()
+                .map { line -> line.split("|").map { it.toInt() }.let { (a, b) -> a to b } }
+                .groupBy({ it.first }, { it.second })
+        val updates =
+            split[1]
+                .lines()
+                .map { line -> line.split(""",\s*""".toRegex()).map { line -> line.toInt() } }
+        return before to updates
+    }
 
-    val sample = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"""
+    private fun isOrdered(
+        before: Map<Int, List<Int>>,
+        input: List<Int>,
+    ): Boolean {
+        val s = input.joinToString(",", prefix = ",", postfix = ",")
+        return input.all { instr ->
+            (before[instr] ?: listOf()).all { b ->
+                b !in input || """,$instr(,\d+)*,$b,""".toRegex().find(s) != null
+            }
+        }
+    }
 
     private fun part1(input: String): Int {
-        val split = input.split("\n\n")
-        val before = split[0].lines().map { line -> line.split("|").map { it.toInt() }.let { (a, b) -> a to b } }.groupBy({it.first},{it.second})
-        val after = before.map { (k, v) -> v to k }.groupBy({ it.first }, { it.second })
-        val updates = split[1].lines().map { line -> line.split(""",\s*""".toRegex()).map { line -> line.toInt() } }
-        println(before)
-        println(after)
-        println(updates)
-        //println(updates.map { it to orderedRaw(before, it) }.joinToString("\n") { "${it}" })
-        val updated = updates.filter { ordered(before, it) }.sumOf {
-            if ((it.size % 2) == 0) {
-                it[it.size / 2 + 1]
-            } else {
-                it[it.size / 2]
-            }
-        }
-        println(updated)
-        return -1
-    }
-
-    private fun ordered(before: Map<Int, List<Int>>, input: List<Int>): Boolean {
-        val s = "," + input.joinToString("") { "$it," }
-        println("$s => ")
-        return input.all { instr ->
-            val positioning = (before[instr] ?: listOf()).all { b ->
-                val rBefore = """,${instr}(,\d+)*,${b},"""
-                val fBefore = rBefore.toRegex().find(s)
-                println("- [$instr] '$rBefore' $fBefore")
-                b !in input || fBefore != null
-            }
-
-            positioning
-        }
-    }
-
-    private fun orderedRaw(before: Map<Int, Int>, after: Map<Int, List<Int>>, input: List<Int>): List<List<Any>> {
-        val x = input.map { instr ->
-            val curr = input.indexOf(instr)
-            val bef = after[instr]?.map { input.indexOf(it) }?.filter { it >= 0 } ?: listOf()
-            val aft = input.indexOf(before[instr])
-            listOf(
-                bef.isEmpty(), bef.all { it < curr }, aft == -1 || aft > curr,
-            )
-        }
-        return x
+        val (before, updates) = parse(input)
+        return updates
+            .filter { isOrdered(before, it) }
+            .sumOf { it[it.size / 2] }
     }
 
     private fun part2(input: String): Int {
-        return -1
+        val (before, updates) = parse(input)
+        return updates
+            .filterNot { isOrdered(before, it) }
+            .map { correct(before, it) }
+            .sumOf { it[it.size / 2] }
+    }
+
+    private tailrec fun correct(
+        before: Map<Int, List<Int>>,
+        input: List<Int>,
+        output: MutableList<Int> = mutableListOf(),
+    ): List<Int> {
+        if (input.isEmpty()) {
+            return output
+        }
+
+        val x = input.first()
+        val bs = (before[x] ?: listOf()).map { output.indexOf(it) }.filter { it >= 0 }
+
+        if (bs.isEmpty() || output.isEmpty()) {
+            output.add(x)
+        } else {
+            output.add(bs.min(), x)
+        }
+
+        return correct(before, input.drop(1), output)
     }
 
     @JvmStatic
