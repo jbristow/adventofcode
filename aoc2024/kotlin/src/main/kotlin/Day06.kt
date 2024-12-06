352e30a3cdcce2c56a5a051e23628f80da363b73
Jon Bristow <jonathan_bristow@intuit.com>
2024-12-06 09:42:55 -0800

    2024 day06 fiddling

--- aoc2024/kotlin/src/main/kotlin/Day06.kt
+++ aoc2024/kotlin/src/main/kotlin/Day06.kt
@@ -4,18 +4,16 @@ import util.Direction
 import util.Point2d
 import util.Point2dRange
 import util.Up
+import java.util.stream.Collectors
 import kotlin.streams.asStream
 
 object Day06 : AdventOfCode() {
     data class Guard(
         var position: Point2d,
         var direction: Direction = Up,
-        val seen: MutableSet<Point2d> = mutableSetOf(position),
-        val loopChecks: MutableSet<Pair<Point2d, Direction>> = mutableSetOf(position to direction),
+        val loopChecks: MutableSet<Pair<Point2d, Direction>> = LinkedHashSet(listOf(position to direction)),
         var hasLooped: Boolean = false,
     ) {
-        val initial = position to direction
-
         fun turn() {
             val newDirection = direction.turnRight()
             direction = newDirection
@@ -26,82 +24,75 @@ object Day06 : AdventOfCode() {
         fun move() {
             val newPosition = position + direction.offset
             position = newPosition
-            seen.add(newPosition)
-            hasLooped = (newPosition to direction) == initial
+            hasLooped = (newPosition to direction) in loopChecks
             loopChecks.add(newPosition to direction)
         }
     }
 
-    fun part1(input: List<String>): Int {
-        val rocks =
-            input
-                .flatMapIndexed { y, line ->
-                    line
-                        .mapIndexed { x, c ->
-                            when (c) {
-                                '#' -> Point2d(x, y)
-                                else -> null
-                            }
-                        }.filterNotNull()
-                }.toSet()
-        val range = Point2dRange(0 until input.first().length, 0 until input.size)
-        val start =
-            input
-                .flatMapIndexed { y, line ->
-                    line.mapIndexed { x, c -> (c to Point2d(x, y)) }
-                }.find { (c, _) -> c == '^' }!!
-                .second
-        val guard = Guard(start)
+    private fun part1(input: List<String>): Int {
+        val rocks = makeObstacles(input)
+        val range = Point2dRange(input.first().indices, input.indices)
+        val start = findStartPosition(input)
 
-        return guard.patrol(rocks, range).distance
+        return Guard(start).patrol(rocks, range).distance
     }
 
-    fun part2(input: List<String>): Long {
-        val rocks =
-            input
-                .flatMapIndexed { y, line ->
-                    line
-                        .mapIndexed { x, c ->
-                            when (c) {
-                                '#' -> Point2d(x, y)
-                                else -> null
-                            }
-                        }.filterNotNull()
-                }.toSet()
-        val start =
-            input
-                .flatMapIndexed { y, line ->
-                    line.mapIndexed { x, c -> (c to Point2d(x, y)) }
-                }.find { (c, _) -> c == '^' }!!
-                .second
-
-        val range = Point2dRange(0 until input.first().length, 0 until input.size)
+    private fun part2(input: List<String>): Long {
+        val rocks = makeObstacles(input)
+        val start = findStartPosition(input)
+        val range = Point2dRange(input.first().indices, input.indices)
 
         val pass1GuardPath = Guard(start).patrol(rocks, range).path
-        return pass1GuardPath
-            .asSequence()
-            .asStream()
-            .parallel()
-            .filter { it !in rocks && it != start }
-            .map {
-                Guard(start).patrol(rocks + it, range)
-            }.filter { it is Looped }
-            .count()
+
+        val loopRocks =
+            pass1GuardPath
+                .asSequence()
+                .asStream()
+                .parallel()
+                .map { it.first }
+                .distinct()
+                .filter { it !in rocks && it != start }
+                .map {
+                    it to Guard(start).patrol(rocks + it, range)
+                }.filter { it.second is Looped }
+                .map { it.first }
+                .collect(Collectors.toSet())
+
+        return loopRocks.stream().count()
     }
 
-    sealed class Outcome(
-        val distance: Int,
-        val path: Set<Point2d>,
-    ) {
-        class OutOfBounds(
-            distance: Int,
-            path: Set<Point2d>,
-        ) : Outcome(distance, path)
+    private fun findStartPosition(input: List<String>) =
+        input
+            .flatMapIndexed { y, line ->
+                line.mapIndexed { x, c -> (c to Point2d(x, y)) }
+            }.find { (c, _) -> c == '^' }!!
+            .second
 
-        class Looped(
-            distance: Int,
-            path: Set<Point2d>,
-        ) : Outcome(distance, path)
+    private fun makeObstacles(input: List<String>) =
+        input
+            .flatMapIndexed { y, line ->
+                line
+                    .mapIndexed { x, c ->
+                        when (c) {
+                            '#' -> Point2d(x, y)
+                            else -> null
+                        }
+                    }.filterNotNull()
+            }.toSet()
+
+    sealed class Outcome {
+        val distance: Int
+            get() = path.map { it.first }.distinct().size
+        abstract val path: Set<Pair<Point2d, Direction>>
+
+        class OutOfBounds(
+            override val path: Set<Pair<Point2d, Direction>>,
+        ) : Outcome()
+
+        data object Looped : Outcome() {
+            override val path: Set<Pair<Point2d, Direction>>
+                get() = throw IllegalStateException("Looped results have no path.")
+        }
     }
 
     private tailrec fun Guard.patrol(
@@ -110,8 +101,8 @@ object Day06 : AdventOfCode() {
     ): Outcome {
         val potentialStep = this.position + this.direction.offset
         return when {
-            hasLooped -> Looped(seen.size, seen)
-            potentialStep !in range -> Outcome.OutOfBounds(seen.size, seen)
+            hasLooped -> Looped
+            potentialStep !in range -> Outcome.OutOfBounds(loopChecks)
             potentialStep in rocks -> {
                 turn()
                 patrol(rocks, range)
